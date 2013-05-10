01a395092463de84ea18cd84f8562ebf714b36bf
yuguo <qiaoyuguo2012@gmail.com>
2013-05-10 12:38:07 +0800

    add initial file

---
+++ purdue-cs240-Spring2011/lab3/mygrep.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int isPresent (char *mainString, char *subString);
+
+/******************************************************************************
+ * main() function
+ ******************************************************************************/
+
+int main(int argc, char **argv){
+
+	//check for arguments
+
+
+	// iterate through all the lines in the input file and display those
+	// which contain the string passed as command line argument
+	
+	
+}
+
+/******************************************************************************
+ * isPresent() function
+ ******************************************************************************/
+
+int isPresent (char *mainString, char *subString){
+
+	// if subString is present in mainString then return 1 else 0
+	
+	return 0;
+}
---
+++ purdue-cs240-Spring2011/lab3/reverse.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void reverse(char *string);
+
+/******************************************************************************
+ * main() function
+ ******************************************************************************/
+
+int main(int argc, char **argv){
+
+	//check for arguments
+	
+	
+	//concatenate the strings
+	
+	
+	//reverse the string
+	
+	
+}
+
+/******************************************************************************
+ * reverse() function using recursion
+ ******************************************************************************/
+
+void reverse(char *string){
+
+}
