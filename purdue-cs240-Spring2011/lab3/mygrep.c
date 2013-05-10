938f4efc2cac8f5dd0348188f6c661ba06a85377
yuguo <qiaoyuguo2012@gmail.com>
2013-05-10 16:38:53 +0800

    finish mygrep

--- purdue-cs240-Spring2011/lab3/mygrep.c
+++ purdue-cs240-Spring2011/lab3/mygrep.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_SIZE 300
+
 int isPresent (char *mainString, char *subString);
 
 /******************************************************************************
@@ -10,13 +12,24 @@ int isPresent (char *mainString, char *subString);
 
 int main(int argc, char **argv){
 
-	//check for arguments
+    char buf[MAX_SIZE] = {0};
+    //check for arguments
+    if(argc != 2)
+    {
+        printf("Usage: mygrep {String to Search} < {File Name}\n");
+        return -1;
+    }
+    
 
-
-	// iterate through all the lines in the input file and display those
-	// which contain the string passed as command line argument
+    // iterate through all the lines in the input file and display those
+    // which contain the string passed as command line argument
+    while(fgets(buf, MAX_SIZE, stdin ) != NULL)
+    {
+        if(isPresent(buf, argv[1]))
+            printf("%s", buf);
+    }
 	
-	
+    return 0;
 }
 
 /******************************************************************************
@@ -24,8 +37,10 @@ int main(int argc, char **argv){
  ******************************************************************************/
 
 int isPresent (char *mainString, char *subString){
-
-	// if subString is present in mainString then return 1 else 0
-	
-	return 0;
+    
+    // if subString is present in mainString then return 1 else 0
+    if(strstr(mainString, subString) == NULL) 
+        return 0;
+    else
+        return 1;
 }
