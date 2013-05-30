c97666bf3de06f0a0eeb8f497f1d7c6976a47857
yuguo <qiaoyuguo2012@gmail.com>
2013-05-30 14:20:43 +0800

    get last row id

---
+++ python-sqlite3-tutor/lastrowid.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# get id of the last inserted row by using lastrowid attribute of the
+# cursor object
+
+import sqlite3 as lite
+import sys
+
+con = lite.connect(':memory:')
+
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT)")
+    cur.execute("INSERT INTO Friends(Name) VALUES ('Tom');")
+    cur.execute("INSERT INTO Friends(Name) VALUES ('Rebecca');")
+    cur.execute("INSERT INTO Friends(Name) VALUES ('Jim');")
+    cur.execute("INSERT INTO Friends(Name) VALUES ('Robert')")
+
+    lid = cur.lastrowid
+    print "The last id of the inserted row is %d" % lid
