d12152407f0337342b070c275f5a1b52a4754908
qiaoyuguo <qiaoyuguo2012@gmail.com>
2013-07-06 16:38:20 +0800

    add bitstarter

---
+++ startup-eng/bitstarter/web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var fs = require('fs')
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  file_buf = fs.readFileSync("index.html");
+  cont = file_buf.toString()
+  response.send(cont);
+  //response.send('Hello World 2!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
