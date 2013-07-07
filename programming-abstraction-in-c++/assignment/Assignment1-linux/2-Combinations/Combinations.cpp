7ee7820c028e2ddb9ffc1acfbafb9e696d175792
qiaoyuguo <qiaoyuguo2012@gmail.com>
2013-07-07 14:24:28 +0800

    remove useless file and finish Combinations

--- programming-abstraction-in-c++/assignment/Assignment1-linux/2-Combinations/Combinations.cpp
+++ programming-abstraction-in-c++/assignment/Assignment1-linux/2-Combinations/Combinations.cpp
@@ -12,7 +12,20 @@
 #include "simpio.h"
 using namespace std;
 
+#define MAX_LINE 10
+
+int combine(int n, int k)
+{
+    if(k == 0 || k == n)
+        return 1;
+    return combine(n-1,k-1) + combine(n-1, k);
+}
 int main() {
-   // [TODO: fill in the code]
+   for(int n = 0; n < MAX_LINE; n++)
+   {
+       for(int i = 0; i <= n; i++)
+           cout << combine(n, i) <<" ";
+       cout << endl;
+    }
    return 0;
 }
