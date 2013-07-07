d5971c89a376855d0e2f9fa20c9c14165bb1b4c2
qiaoyuguo <qiaoyuguo2012@gmail.com>
2013-07-07 14:08:05 +0800

    remove useless file and finish writing ConsecutiveHeads

--- programming-abstraction-in-c++/assignment/Assignment1-linux/1-ConsecutiveHeads/ConsecutiveHeads.cpp
+++ programming-abstraction-in-c++/assignment/Assignment1-linux/1-ConsecutiveHeads/ConsecutiveHeads.cpp
@@ -15,5 +15,24 @@ using namespace std;
 
 int main() {
    // [TODO: fill in the code]
+   int consecutive_heads_num = 0;
+   int total_num = 0;
+   while(consecutive_heads_num < 3)
+   {
+       int rand_num = randomInteger(0,1);
+       ++total_num;
+       if(rand_num == 0)
+       {
+           cout << "heads" <<endl;
+           ++consecutive_heads_num;
+       }
+       else
+       {
+           cout << "tails" <<endl;
+           consecutive_heads_num = 0;
+       }
+   }
+   cout << "It took " <<total_num <<" flips to get " << consecutive_heads_num << " consecutive heads." << endl;
+
    return 0;
 }
