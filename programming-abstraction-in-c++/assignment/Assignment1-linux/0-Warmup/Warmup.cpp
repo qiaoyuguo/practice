f3e0ddcc3674df69347348ff20447584726eb1d6
qiaoyuguo <qiaoyuguo2012@gmail.com>
2013-07-07 13:41:50 +0800

    modifiy Makefile and compile error

--- programming-abstraction-in-c++/assignment/Assignment1-linux/0-Warmup/Warmup.cpp
+++ programming-abstraction-in-c++/assignment/Assignment1-linux/0-Warmup/Warmup.cpp
@@ -27,7 +27,10 @@ int hashCode(string key);
 /* Main program to test the hash function */
 
 int main() {
-   string name = getLine("Please enter your name: ");
+   string name;
+   cout << "Please enter your name: ";
+   cin >>name;
+   //string name = getLine("Please enter your name: ");
    int code = hashCode(name);
    cout << "The hash code for your name is " << code << "." << endl;
    return 0;
@@ -49,7 +52,7 @@ int main() {
 int hashCode(string str) {
    unsigned hash = HASH_SEED;
    int nchars = str.length();
-   for (i = 0; i < nchars; i++) {
+   for (int i = 0; i < nchars; i++) {
       hash = HASH_MULTIPLIER * hash + str[i];
    }
    return (hash & HASH_MASK);
