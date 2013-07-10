dea506c4cba21fa1434c7c04684e9ae88c13c68c
yuguo <qiaoyuguo2012@gmail.com>
2013-07-10 17:11:31 +0800

    add inital commit for exercise1 in chapter4(need to format)

---
+++ programming-abstraction-in-c++/chapter-four/exercises/exercise1/trig.cpp
@@ -0,0 +1,42 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  trig.cpp
+ *
+ *    Description:  produce a sin and cos table for numbers in [-90 90] with step 15
+ *
+ *        Version:  1.0
+ *        Created:  2013年07月10日 16时49分41秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  qiaoyuguo (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "console.h"
+#include "gmath.h"
+
+using namespace std;
+
+void produceTable(void)
+{
+    cout << " theta | sin(theta) | cos(theta) |" <<endl;
+    cout << "---------------------------------+" << endl;
+    for(int i = -90; i <= 90; i += 15) 
+    {
+        cout <<" "<< setw(5) << i << " | " << setw(10) << setprecision(7)<< sinDegrees(i)  << " | " << cosDegrees(i) << " |" << endl;
+    }
+}
+
+int main(void)
+{
+    produceTable();
+
+    return 0;
+}
