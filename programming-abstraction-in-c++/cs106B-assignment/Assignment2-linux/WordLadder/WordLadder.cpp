b44ca0d59a27fbee1a00a5f1f91edca91bbfcc85
yuguo <qiaoyuguo2012@gmail.com>
2013-07-11 16:56:36 +0800

    add initial assignment2 file

---
+++ programming-abstraction-in-c++/cs106B-assignment/Assignment2-linux/WordLadder/WordLadder.cpp
@@ -0,0 +1,22 @@
+/*
+ * File: WordLadder.cpp
+ * --------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the word ladder problem
+ * on Assignment #2.
+ * [TODO: extend the documentation]
+ */
+
+#include <iostream>
+#include "console.h"
+#include "lexicon.h"
+#include "queue.h"
+#include "simpio.h"
+#include "vector.h"
+using namespace std;
+
+int main() {
+	// [TODO: fill in the code]
+	return 0;
+}
