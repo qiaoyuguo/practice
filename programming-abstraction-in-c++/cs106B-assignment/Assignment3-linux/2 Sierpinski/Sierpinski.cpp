d28672319375666617ef909229a57bb9d6ed2fe5
yuguo <qiaoyuguo2012@gmail.com>
2013-07-12 15:52:32 +0800

    finish Sierpinski of Assignment3(CS106B)

--- programming-abstraction-in-c++/cs106B-assignment/Assignment3-linux/2 Sierpinski/Sierpinski.cpp
+++ programming-abstraction-in-c++/cs106B-assignment/Assignment3-linux/2 Sierpinski/Sierpinski.cpp
@@ -7,12 +7,41 @@
  * on Assignment #3.
  * [TODO: extend the documentation]
  */
-
+#include <cmath>
 #include <iostream>
+#include "simpio.h"
 #include "gwindow.h"
+
 using namespace std;
 
-int main() {
-	// [TODO: fill in the code]
-	return 0;
+const double SIZE = 400;
+
+// pt is the downleft point of triangle, size is the triangle size
+void sierpinski(GWindow gw, GPoint pt, double size, int order);
+
+int main(void)
+{
+    GWindow gw(600, 600);
+    double cx = gw.getWidth()/2;
+    double cy = gw.getHeight()/2; 
+    GPoint pt(cx-SIZE/2, cy+SIZE*sqrt(3)/6);
+    double edgeLen = getReal("Please input edge length[1,400]:");
+    int order = getInteger("Please input order[1,10]:");
+    sierpinski(gw, pt, edgeLen, order);
+
+    return 0;
+}
+
+void sierpinski(GWindow gw, GPoint pt, double size, int order)
+{
+    if(order == 0)
+    {
+        pt = gw.drawPolarLine(pt, size, 0);
+        pt = gw.drawPolarLine(pt, size, 120 );
+        pt = gw.drawPolarLine(pt, size, -120);
+        return ;
+    }
+    sierpinski(gw,pt, size/2, order-1);
+    sierpinski(gw, GPoint(pt.getX()+size/2, pt.getY()), size/2, order-1);
+    sierpinski(gw, GPoint(pt.getX()+size/4, pt.getY()-size*sqrt(3)/4), size/2, order-1);
 }
