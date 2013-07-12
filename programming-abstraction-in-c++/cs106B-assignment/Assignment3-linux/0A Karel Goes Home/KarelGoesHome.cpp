52f6d09958a4225d9767f7ebfac277bd50bb6700
yuguo <qiaoyuguo2012@gmail.com>
2013-07-12 15:00:55 +0800

    add initial Assignment3 files

---
+++ programming-abstraction-in-c++/cs106B-assignment/Assignment3-linux/0A Karel Goes Home/KarelGoesHome.cpp
@@ -0,0 +1,24 @@
+/*
+ * File: KarelGoesHome.cpp
+ * ----------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the Karel Goes Home
+ * warmup problem for Assignment 3
+ * [TODO: extend the documentation]
+ */
+
+#include <iostream>
+#include "console.h"
+using namespace std;
+
+/* Given a street number and avenue number, which are 1-indexed,
+ * returns the number of paths Karel can take back home that only
+ * moves left or down.
+ */
+int numPathsHome(int street, int avenue);
+
+int main() {
+	// [TODO: fill in the code]
+	return 0;
+}
