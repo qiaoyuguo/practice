473bdcaabd04a8f60e345b164d8ed76b2d5886ba
yuguo <qiaoyuguo2012@gmail.com>
2013-07-24 16:34:58 +0800

    add solution for hw2 1-2,4-6

--- coding-matrix/week2/hw2.py
+++ coding-matrix/week2/hw2.py
@@ -3,8 +3,6 @@
 
 from vec import Vec
 
-
-
 ## Problem 1
 def vec_select(veclist, k): 
     '''
@@ -16,7 +14,11 @@ def vec_select(veclist, k):
     >>> vec_select([v1, v2, v3, v4], 'a') == [Vec(D,{'b': 1}), Vec(D,{'b': 2})]
     True
     '''
-    pass
+    result = []
+    for v in veclist:
+        if v[k] == 0:
+            result.append(v)
+    return result
 
 def vec_sum(veclist, D): 
     '''
@@ -28,7 +30,12 @@ def vec_sum(veclist, D):
     >>> vec_sum([v1, v2, v3, v4], D) == Vec(D, {'b': 13, 'a': 11})
     True
     '''
-    pass
+    result = Vec(D.copy(),dict())
+    for d in D:
+        result[d] = 0
+        for v in veclist:
+            result[d] += v[d]
+    return result
 
 def vec_select_sum(veclist, k, D): 
     '''
@@ -40,7 +47,7 @@ def vec_select_sum(veclist, k, D):
     >>> vec_select_sum([v1, v2, v3, v4], 'a', D) == Vec(D, {'b': 3})
     True
     '''
-    pass
+    return vec_sum(vec_select(veclist, k), D)
 
 
 
@@ -52,7 +59,13 @@ def scale_vecs(vecdict):
     >>> scale_vecs({3: v1, 5: v2}) == [Vec({1,2,3},{2: 3.0}), Vec({1,2,4},{1: 0.2, 2: 0.4, 4: 1.6})]
     True
     '''
-    pass
+    for scale in vecdict.keys():
+        v = vecdict[scale]
+        for d in v.D:
+            if v[d] != 0:
+                v[d] /= scale
+    #print(list(vecdict.values()))
+    return list(vecdict.values())
 
 
 
@@ -73,24 +86,35 @@ def GF2_span(D, L):
     >>> Vec(D, {x:one for x in D}) in GF2_span(D, L)
     True
     '''
-    pass
-
+    lst = []
+    for v in L:
+        for i in v.f:
+            lst.append(tuple([i,v.f[i]]))
+    print(lst)
+    import itertools
+    #for i in itertools.permutations(lst):
+    #    print(i)
+    return ...
 
 
 ## Problem 4
 # Answer with a boolean, please.
 
-is_it_a_vector_space_1 = ...
-is_it_a_vector_space_2 = ...
+is_it_a_vector_space_1 = True
+is_it_a_vector_space_2 = False
 
 
 
 ## Problem 5
-is_it_a_vector_space_3 = ...
-is_it_a_vector_space_4 = ...
+is_it_a_vector_space_3 = True
+is_it_a_vector_space_4 = False
 
 
 ## Problem 6
 
-is_it_a_vector_space_5 = ...
-is_it_a_vector_space_6 = ...
+is_it_a_vector_space_5 = True
+is_it_a_vector_space_6 = False
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
