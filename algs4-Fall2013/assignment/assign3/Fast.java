bb459d09b77aba71cb1875b423fa03039fc00b83
yuguo <qiaoyuguo2012@gmail.com>
2013-09-23 16:00:58 +0800

    arrive 94.58%

--- algs4-Fall2013/assignment/assign3/Fast.java
+++ algs4-Fall2013/assignment/assign3/Fast.java
@@ -1,5 +1,6 @@
 import java.util.Collections;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Fast
 {
@@ -9,16 +10,23 @@ public class Fast
         int N = infile.readInt(); 
         StdDraw.setXscale(0, 32768) ;
         StdDraw.setYscale(0, 32768);
-        ArrayList<Point> pts = new ArrayList<Point>();
+        Point []pts = new Point[N];
+        Point []new_pts = new Point[N];
+        //double []slopes = new double[N];
+        //int count = 0;
+        //ArrayList<Point> pts = new ArrayList<Point>();
         for(int i = 0; i < N; i++)
         {
             int x = infile.readInt();
             int y = infile.readInt();
-            Point curPt = new Point(x,y);
-            pts.add(curPt);
-            curPt.draw();
+            pts[i] = new Point(x,y);
+            new_pts[i] = pts[i];
+            //Point curPt = new Point(x,y);
+            //pts.add(curPt);
+            //curPt.draw();
         }
-        Collections.sort(pts);
+        Arrays.sort(pts);
+        Arrays.sort(new_pts);
         
         /* 
         for(int i = 0; i < N; i++)
@@ -27,56 +35,85 @@ public class Fast
         */
         
         
-        ArrayList<Point> new_pts = (ArrayList<Point>)pts.clone();
-        boolean []visited = new boolean[N];
-        for(int i = 0; i < N; i++)
+        //ArrayList<Point> new_pts = (ArrayList<Point>)pts.clone();
+        //boolean []visited = new boolean[N];
+        for(int i = 0; i < N-1; i++)
         {
-            Point cur_point = pts.get(i);
-            Collections.sort(new_pts);
-            Collections.sort(new_pts, cur_point.SLOPE_ORDER);
+            Point cur_point = pts[i];
+            //Collections.sort(new_pts);
+            Arrays.sort(new_pts);
+            Arrays.sort(new_pts,i+1, N,  cur_point.SLOPE_ORDER);
+            //Arrays.sort( new_pts, cur_point.SLOPE_ORDER);
+            double []slopes = new double[N];
+            for(int idxs = 0; idxs < N; idxs++)
+                slopes[idxs] = cur_point.slopeTo(pts[idxs]);
+            //Collections.sort(new_pts, cur_point.SLOPE_ORDER);
 
             //debug
-            /*
+            /* 
             System.out.print(i+","+cur_point+" ");
             for(int m = 0; m < N; m++)
-                System.out.print(new_pts.get(m) + "," + cur_point.slopeTo(new_pts.get(m)) + " ");
+                System.out.print(new_pts[m] + "," + cur_point.slopeTo(new_pts[m]) + " ");
+                //System.out.print(new_pts.get(m) + "," + cur_point.slopeTo(new_pts.get(m)) + " ");
             System.out.println();
             */
+            
 
-            int j = 0; 
+            int j = i+1; 
             while(j < N)
             {
-                Point start_point = new_pts.get(j);
+                Point start_point = new_pts[j];
+                //if(cur_point.slopeTo(new Point(30000,0)) == Double.NEGATIVE_INFINITY)
+                //    StdOut.println("start point " + start_point);
                 double cur_slope = cur_point.slopeTo(start_point);
                 int k;
                 for(k = j+1; k < N; k++)
                 {
-                    if(cur_point.slopeTo(new_pts.get(k)) != cur_slope)
+                    //if(cur_point.slopeTo(new_pts.get(k)) != cur_slope)
+                    if(cur_point.slopeTo(new_pts[k]) != cur_slope)
                         break;
                 }
-                if(k - j < 2)
+                if(k - j < 3)
                 {
                     j = k;
                     continue;
                 }
+                /*
                 int idx = pts.indexOf(start_point);
                 if(visited[idx])
                 {
                     j = k;
                     continue;
                 }
-                visited[pts.indexOf(cur_point)] = true;
+                */
+                //visited[pts.indexOf(cur_point)] = true;
                 //StdOut.print(i + " " + cur_point + "=>");
+                boolean found = false;
+                int schIdx;
+                for(schIdx = 0; schIdx < i; schIdx ++)
+                    if(slopes[schIdx] == cur_slope)
+                    {
+                        found = true;
+                        break;
+                    }
+                //StdOut.println("find in left array" + cur_point + " " + slopes[schIdx]);
+                if(found)
+                {
+                    j = k;
+                    continue;
+                }
                 StdOut.print(cur_point);
                 //cur_point.draw();
                 for(int l = j; l < k; l++)
                 {
-                    Point tmpPoint = new_pts.get(l);
-                    visited[pts.indexOf(tmpPoint)] = true;
-                    StdOut.print( " -> " + tmpPoint);
+                    //Point tmpPoint = new_pts.get(l);
+                    //visited[pts.indexOf(tmpPoint)] = true;
+                    //StdOut.print( " -> " + tmpPoint);
+                    StdOut.print( " -> " + new_pts[l]);
                     //tmpPoint.draw();
                 }
-                cur_point.drawTo(new_pts.get(k-1));
+                //cur_point.drawTo(new_pts.get(k-1));
+                cur_point.drawTo(new_pts[k-1]);
 
                 StdOut.println();
                 j = k;
