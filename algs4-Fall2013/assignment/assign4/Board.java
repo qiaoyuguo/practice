42ad0ed2ca27ec4ff592668f1d6ce46ed898068c
yuguo <qiaoyuguo2012@gmail.com>
2013-09-26 16:08:11 +0800

    add week4 solution(without debugging)

---
+++ algs4-Fall2013/assignment/assign4/Board.java
@@ -0,0 +1,152 @@
+import java.util.Iterator;
+
+public class Board {
+    private int N;
+    private int [][]board;
+    public Board(int[][] blocks)           // construct a board from an N-by-N array of blocks
+    {
+        N = blocks.length;
+        board = new int[N][N];
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+                board[i][j] = blocks[i][j];
+    }
+                                           // (where blocks[i][j] = block in row i, column j)
+    public int dimension()                 // board dimension N
+    {
+        return N;
+    }
+    public int hamming()                   // number of blocks out of place
+    {
+        int count = 0;
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+            {
+                if(board[i][j] != i+j+1)
+                    ++count;
+            }
+        return count;
+    }
+    public int manhattan()                 // sum of Manhattan distances between blocks and goal
+    {
+        int count = 0;
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+            {
+                int x = (board[i][j]-1)/N;
+                int y = (board[i][j]-1)%N;
+                count += Math.abs(x-i) + Math.abs(y-j);
+            }
+        return count;
+    }
+    public boolean isGoal()                // is this board the goal board?
+    {
+        for(int i = 0; i < N; i++)
+            for(int  j = 0; j < N; j++)
+            {
+                if(i == N && j == N)
+                {
+                    if(board[i][j] != 0)
+                        return false;
+                }
+                else if(board[i][j] != i+j+1)
+                    return false;
+            }
+        return true;
+    }
+    public Board twin()                    // a board obtained by exchanging two adjacent blocks in the same row
+    {
+        Board newBoard = new Board(board);
+        int i = 0;
+        int j1= 0, j2 = 1;
+        if(newBoard.board[i][j1] == 0)
+            j1 = 2;
+        else if(newBoard.board[i][j2] == 0)
+            j2 = 2;
+        int tmp = newBoard.board[i][j1];
+        newBoard.board[i][j1] = newBoard.board[i][j2];
+        newBoard.board[i][j2] = tmp;
+
+        return newBoard;
+    }
+    public boolean equals(Object y)        // does this board equal y?
+    {
+        if(y == this) return true;
+        if(y == null) return false;
+        if(y.getClass() != this.getClass()) return false;
+        Board that = (Board)y;
+        if(this.N != that.N)
+            return false;
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+                if(board[i][j] != that.board[i][j] )
+                    return false;
+        return true;
+    }
+    public Iterable<Board> neighbors()     // all neighboring boards
+    {
+        Queue<Board> queue = new Queue<Board>();
+        //StdOut.println("N =" + N);
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N-1; j++)
+            {
+                if(board[i][j] == 0 || board[i][j+1] == 0)
+                    continue;
+                Board newBoard = new Board(board);
+                //StdOut.println(newBoard);
+                int tmp = newBoard.board[i][j];
+                newBoard.board[i][j] = newBoard.board[i][j+1];
+                newBoard.board[i][j+1] = tmp;
+                queue.enqueue(newBoard);
+            }
+
+        return queue;
+    }
+    public String toString()               // string representation of the board (in the output format specified below)
+    {
+        StringBuilder s = new StringBuilder();
+        s.append(N + "\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(board[i][j] != 0)
+                {
+                    s.append(String.format("%2d ", board[i][j]));
+                }
+                else
+                {
+                    s.append(String.format("   ", board[i][j]));
+                }
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+    public static void main(String[] args)
+    {
+        int [][]blocks = new int[][]{{0, 1,3},{4,2,5},{7,8,6}};
+        int [][]blocks1 = new int[][]{{0, 1,3},{4,2,5},{7,8,6}};
+        int [][]blocks2 = new int[][]{{1, 2,3},{4,5,6},{7,8,0}};
+        StdOut.println(blocks);
+        Board brd = new Board(blocks);
+        Board brd1 = new Board(blocks1);
+        Board brd2 = new Board(blocks2);
+        StdOut.println(brd);
+        assert(brd.dimension() == 3);
+        assert(brd.hamming() == 5);
+        assert(brd.manhattan() == 10);
+        assert(brd.isGoal() == false);
+        assert(brd2.isGoal() == true);
+        assert(brd.equals(brd1) == true);
+        assert(brd.equals(brd2) == false);
+        StdOut.print(brd2);
+
+        Iterable<Board> qe= brd.neighbors();
+        Iterator it = qe.iterator();
+        StdOut.println(it.hasNext());
+        while(it.hasNext())
+        {
+            StdOut.println(it.next());
+        }
+    }
+
+}
---
+++ algs4-Fall2013/assignment/assign4/Solver.java
@@ -0,0 +1,111 @@
+import java.util.Iterator;
+public class Solver {
+    private class SearchNode 
+    {
+        private Board brd;
+        private int moves;
+        private int priority;
+        private SearchNode prev;
+    }
+    private Queue<SearchNode> initQueue;
+    private Queue<SearchNode> twinQueue;
+    private Queue<SearchNode> GetSolution(Board initial)
+    {
+        Queue<SearchNode> queue = new Queue<SearchNode>();
+        MinPQ<SearchNode> pq = new MinPQ<SearchNode>();
+        SearchNode node = new SearchNode();
+        node.brd = initial;
+        node.moves = 0;
+        node.priority = initial.manhattan();
+        node.prev = null;
+        pq.insert(node);
+        SearchNode curNode = null;
+        while(!pq.isEmpty())
+        {
+            curNode = pq.delMin();
+            if(curNode.brd.isGoal())
+                break;
+            Iterable<Board> qe= curNode.brd.neighbors();
+            Iterator it = qe.iterator();
+            while(it.hasNext())
+            {
+                node.brd = (Board)it.next();  
+                node.moves = curNode.moves + 1;
+                node.prev = curNode;
+                node.priority = node.brd.manhattan();
+                queue.enqueue(node);
+            }
+        }
+        if(queue.isEmpty())
+            return null;
+        else
+        {
+            while(queue != null)
+            {
+                queue.enqueue(curNode);
+                curNode = curNode.prev;
+            }
+            return queue;
+        }
+            
+    }
+    public Solver(Board initial)            // find a solution to the initial board (using the A* algorithm)
+    {
+        initQueue = GetSolution(initial);
+        twinQueue = GetSolution(initial.twin());
+    }
+    public boolean isSolvable()             // is the initial board solvable?
+    {
+        if(initQueue != null || twinQueue != null)
+            return true;
+        else
+            return false;
+    }
+    public int moves()                      // min number of moves to solve initial board; -1 if no solution
+    {
+        if(initQueue != null)
+            return initQueue.peek().moves;
+        else if(twinQueue != null)
+            return twinQueue.peek().moves;
+        else
+            return -1;
+    }
+    public Iterable<Board> solution()       // sequence of boards in a shortest solution; null if no solution
+    {
+        Queue<Board> queue = null;
+        Queue<SearchNode> nodeQueue = new Queue<SearchNode>();
+        if(initQueue != null)
+            nodeQueue = initQueue;
+        else if(twinQueue != null)
+            nodeQueue = twinQueue;
+        else
+            return null;
+
+        for(SearchNode node: nodeQueue)
+        {
+            queue.enqueue(node.brd);
+        }
+        return queue;
+    }
+    public static void main(String[] args)  // solve a slider puzzle (given below)
+    {
+        In in = new In(args[0]);
+        int N = in.readInt();
+        int [][]blocks = new int[N][N];
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+            {
+                blocks[i][j] = in.readInt();
+            }
+
+        Board brd = new Board(blocks);
+        Solver slv = new Solver(brd);
+        Iterable<Board> queue = slv.solution();
+        Iterator it = queue.iterator();
+        StdOut.printf("Minimum number of moves = %d\n", slv.moves());
+        while(it.hasNext())
+        {
+            StdOut.println(it.next());
+        }
+    }
+}
